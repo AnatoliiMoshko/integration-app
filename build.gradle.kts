c30d6a431c836dd9e28304c76e933c2569c97bc0
Anatolii Moshko <anatoliimoshko@gmail.com>
2024-03-04 12:33:47 +0300

    added dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ extra["springCloudVersion"] = "2023.0.0"
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
-
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("com.squareup.okhttp3:okhttp:4.11.0")
 
 	compileOnly("org.projectlombok:lombok")
